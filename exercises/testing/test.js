b3fa58ea9edbc7d9bda4b58643b847b583eed290
tangkaiqiang <justforgit@163.com>
2020-09-08 21:14:27 +0800

    debug and test solution

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,13 @@
+const users = require('./users');
+const { fixId } = require('./users');
+
 // write some tests
 describe('users', () => {
-  
-})
+  test('findUser', async (done) => {
+    expect(await user.findUser('1')).toBe(1);
+    done();
+  });
+  test('fixId', () => {
+    expect(fixId('100')).toBe(100);
+  });
+});
